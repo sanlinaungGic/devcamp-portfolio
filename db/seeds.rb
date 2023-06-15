b890ad3e4076786221f7333702f74cb317d80890
sanlinaungGic <sanlinaung@gicjp.com>
2023-06-15 10:01:46 +0630

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu nisl nunc mi ipsum.
+             Faucibus ornare suspendisse sed nisi. Feugiat pretium nibh
+             ipsum consequat nisl vel pretium. Egestas dui id ornare arcu 
+             odio ut sem. Tortor pretium viverra suspendisse potenti nullam. Elit ullamcorper dignissim cras tincidunt
+              lobortis feugiat. Enim blandit volutpat maecenas volutpat blandit. Posuere morbi leo urna molestie at elementum. Ultricies
+               tristique nulla aliquet enim tortor at auctor urna.
+               Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Pharetra diam sit amet nisl suscipit
+                adipiscing bibendum est. Sed nisi lacus sed viverra tellus in hac habitasse."
+    )
+end
+
+puts "10 blogs created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "My great service",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu nisl nunc mi ipsum.
+            Faucibus ornare suspendisse sed nisi. Feugiat pretium nibh
+            ipsum consequat nisl vel pretium.",
+        main_image: "https://place-hold.it/600x400",
+        thumb_image:  "https://place-hold.it/350x200"
+    )
+end
+
+puts "9 portfolios created"
