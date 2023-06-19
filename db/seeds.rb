73db8beb3ce5354eaa2296c298e29c955a6acb85
sanlinaungGic <sanlinaung@gicjp.com>
2023-06-19 11:00:53 +0630

    implemented custom scopes for portfolio itesm

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end 
+
+puts "3 Topics created"
+
 10.times do |blog|
     Blog.create!(
         title: "My Blog Post #{blog}",
@@ -9,7 +17,8 @@
               lobortis feugiat. Enim blandit volutpat maecenas volutpat blandit. Posuere morbi leo urna molestie at elementum. Ultricies
                tristique nulla aliquet enim tortor at auctor urna.
                Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Pharetra diam sit amet nisl suscipit
-                adipiscing bibendum est. Sed nisi lacus sed viverra tellus in hac habitasse."
+                adipiscing bibendum est. Sed nisi lacus sed viverra tellus in hac habitasse.",
+        topic_id: Topic.last.id
     )
 end
 
@@ -24,10 +33,22 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
-        subtitle: "My great service",
+        subtitle: "Ruby on Rails",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu nisl nunc mi ipsum.
+            Faucibus ornare suspendisse sed nisi. Feugiat pretium nibh
+            ipsum consequat nisl vel pretium.",
+        main_image: "https://place-hold.it/600x400",
+        thumb_image:  "https://place-hold.it/350x200"
+    )
+end
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Angular",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu nisl nunc mi ipsum.
             Faucibus ornare suspendisse sed nisi. Feugiat pretium nibh
