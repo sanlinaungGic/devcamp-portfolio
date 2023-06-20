84417096a5bad7b4604efe42238ff8ba77b24773
sanlinaungGic <sanlinaung@gicjp.com>
2023-06-20 08:17:02 +0630

    Customized registration, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, paht: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, expect: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
