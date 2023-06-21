61399135b081e65c6936b1c3f6bef548ce4645b4
sanlinaungGic <sanlinaung@gicjp.com>
2023-06-21 23:15:36 +0630

    final of rubygem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include DeviseWhitelist   
-    
     include SetSource
-
     include CurrentUserConcern
-
     include DefaultPageContent 
+
+    before_action :set_copyright
+
+    def set_copyright
+        @copyright = SannViewTool::Renderer.copyright 'San Lin Aung', 'All rights reserved.'
+    end
+end
+
+module SannViewTool
+    class Renderer
+        def self.copyright name, msg
+            "&copy: #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+        end
+    end
 end
